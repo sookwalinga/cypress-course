a6a0634a439871be3c2e4d026fb5c0978d6395c1
sam <sookwalinga@gmail.com>
2023-09-12 12:31:40 +0000

    12 Sept 2023, 15:31

--- cypress/component/Accordion.cy.jsx
+++ cypress/component/Accordion.cy.jsx
@@ -4,6 +4,8 @@ const { items } = require('@/app/examples/page')
 describe('Accordion.cy.jsx', () => {
   it('Items accordion', () => {
     cy.mount(<ItemsAccordion items={items} />)
-    cy.getDataTest('accordion-wrap').within(() => {})
+    cy.getDataTest('accordion-wrap').within(() => {
+      cy.get('[data-test^="accordion-item"]').should('have.length', 4)
+    })
   })
 })
