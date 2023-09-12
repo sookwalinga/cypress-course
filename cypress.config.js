63f1cd89195bd3aad510f43d831ba3157e41d69e
sam <sookwalinga@gmail.com>
2023-09-12 12:23:27 +0000

    12 Sept 2023, 15:23

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,17 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-})
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
