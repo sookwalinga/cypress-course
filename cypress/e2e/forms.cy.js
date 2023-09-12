e5bda5e234d953de19bc15963e9cd909608e243f
sam <sookwalinga@gmail.com>
2023-09-12 10:28:12 +0000

    12 Sept 2023, 13:28

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -2,6 +2,6 @@ describe('Forms test', () => {
   beforeEach(() => cy.visit('/forms'))
   it('Test subscribe form', () => {
     cy.contains(/testing forms/i)
-    cy.getDataTest('subscribe-form')
+    cy.getDataTest('subscribe-form').find('input').type('sookwalinga@gmail.com')
   })
 })
