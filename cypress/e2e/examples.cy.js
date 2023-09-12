54cf9e5ac4fdd524d2fbd68a98138d757c373bcc
sam <sookwalinga@gmail.com>
2023-09-12 11:20:52 +0000

    12 Sept 2023, 14:20

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -1,6 +1,6 @@
 describe('Various examples', () => {
-  beforeEach(() => cy.visit('/examples'))
-  it('multi', () => {
-    cy.contains(/examples/i)
+  // beforeEach(() => cy.visit('/examples'))
+  it('multi-page testing', () => {
+    cy.visit('/').pause()
   })
 })
