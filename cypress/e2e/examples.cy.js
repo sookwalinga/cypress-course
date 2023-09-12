28ad43cca285a843ba9457fe6794f387ecad84a1
sam <sookwalinga@gmail.com>
2023-09-12 12:15:32 +0000

    12 Sept 2023, 15:15

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -55,5 +55,17 @@ describe('Various best-practices', () => {
       cy.get('li').should('have.length', 2)
       cy.get('li').its(0).should('contains.text', 'some grudge')
     })
+
+    cy.getDataTest('grudge-list').within(() => {
+      cy.get('li')
+        .its(0)
+        .within(() => {
+          cy.get('button').click()
+        })
+    })
+
+    cy.getDataTest('grudge-list').within(() => {
+      cy.get('li').should('have.length', 1)
+    })
   })
 })
