97186b2fb879c6554de6fb7de1d3ee2f3a698cbd
sam <sookwalinga@gmail.com>
2023-09-12 11:13:36 +0000

    12 Sept 2023, 14:13

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -2,11 +2,13 @@ describe('Forms test', () => {
   beforeEach(() => cy.visit('/forms'))
   it('Test subscribe form', () => {
     cy.contains(/testing forms/i)
+
     cy.getDataTest('subscribe-form').find('input').as('subscribe-input')
     cy.get('@subscribe-input').type('sookwalinga@gmail.com')
     cy.contains(/Successfully subscribed: sookwalinga@gmail.com!/i).should(
       'not.exist'
     )
+
     cy.getDataTest('subscribe-button').click()
     cy.contains(/Successfully subscribed: sookwalinga@gmail.com!/i).should(
       'exist'
@@ -15,10 +17,15 @@ describe('Forms test', () => {
     cy.contains(/Successfully subscribed: sookwalinga@gmail.com!/i).should(
       'not.exist'
     )
+
     cy.get('@subscribe-input').type('sookwalinga@gmail.io')
     cy.getDataTest('subscribe-button').click()
     cy.contains(/Invalid email: sookwalinga@gmail.com!/i).should('exist')
     cy.wait(3000)
     cy.contains(/Invalid email: sookwalinga@gmail.com!/i).should('not.exist')
+
+    cy.contains(/fail!/i).should('exist')
+    cy.getDataTest('subscribe-button').click()
+    cy.contains(/fail!/i).should('exist')
   })
 })
